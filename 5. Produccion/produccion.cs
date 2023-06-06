c98bd3a0e9db77726736ea6a5284976cf27f1f98
kokushiboeldev <kokushiboeldev@gmail.com>
2023-06-05 21:59:58 -0400

    produccion agregada

---
+++ 5. Produccion/produccion.cs
@@ -0,0 +1,52 @@
+[Mugen no Yunna]
+[Carolina.bussines@gmail.com]
+[05/06/2023]
+
+Documento de Producción
+
+Número de orden: [#242]
+Fecha de solicitud: [05/06/2023]
+Fecha de entrega: [05/07/2023]
+
+Detalles del producto:
+
+Nombre del producto: [Tic Tac Toe - ]
+Cantidad solicitada: [1 Game]
+Especificaciones técnicas: [Python, developer, ]
+
+Desarrolladores :
+
+- [Desarrollador 1]: Categoria: [Categoria]
+- [Desarrollador 2]: Categoria: [Categoria]
+- [Desarrollador 3]: Categoria: [Categoria]
+- [Desarrollador 4]: Categoria: [Categoria]
+- [Desarrollador 5]: Categoria: [Categoria]
+
+Proceso de producción:
+
+1. Etapa 1: [Descripción de la primera etapa de producción]
+   - Responsable: [Nombre del responsable]
+   - Fecha de inicio: [Fecha de inicio]
+   - Fecha de finalización: [Fecha de finalización]
+
+2. Etapa 2: [Descripción de la segunda etapa de producción]
+   - Responsable: [Nombre del responsable]
+   - Fecha de inicio: [Fecha de inicio]
+   - Fecha de finalización: [Fecha de finalización]
+
+3. Etapa 3: [Descripción de la tercera etapa de producción]
+   - Responsable: [Nombre del responsable]
+   - Fecha de inicio: [Fecha de inicio]
+   - Fecha de finalización: [Fecha de finalización]
+
+4. Etapa 4: [Descripción de la cuarta etapa de producción]
+   - Responsable: [Nombre del responsable]
+   - Fecha de inicio: [Fecha de inicio]
+   - Fecha de finalización: [Fecha de finalización]
+
+5. Etapa 5: [Descripción de la quinta etapa de producción]
+   - Responsable: [Nombre del responsable]
+   - Fecha de inicio: [Fecha de inicio]
+   - Fecha de finalización: [Fecha de finalización]
+
+Instrucciones adicionales:
