7a09fc870682a7609ca7023e018c1a27405b35a2
kokushiboeldev <kokushiboeldev@gmail.com>
2023-06-05 23:31:56 -0400

    Agregados cambios a produccion

--- 5. Produccion/produccion.cs
+++ 5. Produccion/produccion.cs
@@ -12,41 +12,50 @@ Detalles del producto:
 
 Nombre del producto: [Tic Tac Toe - ]
 Cantidad solicitada: [1 Game]
-Especificaciones técnicas: [Python, developer, ]
+Especificaciones técnicas: [Python, developer] 
 
 Desarrolladores :
 
-- [Desarrollador 1]: Categoria: [Categoria]
-- [Desarrollador 2]: Categoria: [Categoria]
-- [Desarrollador 3]: Categoria: [Categoria]
-- [Desarrollador 4]: Categoria: [Categoria]
-- [Desarrollador 5]: Categoria: [Categoria]
+- [Desarrollador 1]: Flavio Henriquez / Amnesia Categoria: [Optimizador]
+- [Desarrollador 2]: Carolina Retamal / Paimon Categoria: [Diseñadora]
+- [Desarrollador 3]: Javier Reyes / Kamikaze Categoria: [Logico]
+- [Desarrollador 4]: Damian Cespedes / X Categoria: [x] #en duda
+- [Desarrollador 5]: Cristobal Mora / Kokushibo Categoria: [Migaja]
+
+Testers:
+
+- Desarrollador 1 , 2 , 3 , 5 x Dia y funcion.
 
 Proceso de producción:
 
-1. Etapa 1: [Descripción de la primera etapa de producción]
-   - Responsable: [Nombre del responsable]
-   - Fecha de inicio: [Fecha de inicio]
-   - Fecha de finalización: [Fecha de finalización]
+1. Etapa 1: [Crear funcionamiento basico y toda interfaz basica]
+   - Responsable: [Todo el equipo]
+   - Fecha de inicio: [06/06/2023]
+   - Fecha de finalización: [12/06/2023]
 
-2. Etapa 2: [Descripción de la segunda etapa de producción]
-   - Responsable: [Nombre del responsable]
-   - Fecha de inicio: [Fecha de inicio]
-   - Fecha de finalización: [Fecha de finalización]
+2. Etapa 2: [Modificar Diseño y estructura de interfaz]
+   - Responsable: [Paimon]
+   - Fecha de inicio: [12/06/2023]
+   - Fecha de finalización: [16/06/2023]
 
-3. Etapa 3: [Descripción de la tercera etapa de producción]
-   - Responsable: [Nombre del responsable]
-   - Fecha de inicio: [Fecha de inicio]
-   - Fecha de finalización: [Fecha de finalización]
+3. Etapa 3: [Revision y optimizacion de la logica]
+   - Responsable: [Kamikaze, Amnesia]
+   - Fecha de inicio: [12/06/2023]
+   - Fecha de finalización: [20/06/2023]
 
-4. Etapa 4: [Descripción de la cuarta etapa de producción]
-   - Responsable: [Nombre del responsable]
-   - Fecha de inicio: [Fecha de inicio]
-   - Fecha de finalización: [Fecha de finalización]
+4. Etapa 4: [Prueba y Arreglo Alpha 0.5]
+   - Responsable: [Kokushibo]
+   - Fecha de inicio: [12/06/2023]
+   - Fecha de finalización: [20/06/2023]
 
-5. Etapa 5: [Descripción de la quinta etapa de producción]
-   - Responsable: [Nombre del responsable]
-   - Fecha de inicio: [Fecha de inicio]
-   - Fecha de finalización: [Fecha de finalización]
+5. Etapa 5: [Empaquetar y Terminar - Final Test]
+   - Responsable: [Todo el equipo]
+   - Fecha de inicio: [21/06/2023]
+   - Fecha de finalización: [26/06/2023]
 
 Instrucciones adicionales:
+
+# se puede utilizar I.A solo para aprendizaje o prueba y error.
+# todo tiene tiempo limite.
+# deben actualizar su githhub o carpeta de githhub con un commet o commit [ comentario de lo que hicieron y pruebas ]
+# Luego si la aplicacion funciona estable de manera correcta en windows - se portara a html web - Android.
